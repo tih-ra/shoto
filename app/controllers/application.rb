48ac662cd3385826c15d3b98555f01c7722a2adc
Andriy Bazyuta <tihhra@gmail.com>
2008-11-07 14:39:04 +0200

    rutils

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,7 +1,7 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-#require "rutils"
-#RuTils::overrides = false
+require "rutils"
+RuTils::overrides = false
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   helper :all # include all helpers, all the time
