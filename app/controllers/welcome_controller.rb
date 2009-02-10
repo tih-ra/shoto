15753534aae4808d5e9ee477e0bbadf6d6011602
Andriy Bazyuta <tihhra@gmail.com>
2009-02-10 19:53:52 +0200

    d

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @posts = Post.paginate(:page => params[:page], :per_page => 2, :conditions=>{:is_public=>true}, :order=>"posts.created_at DESC")
+    @posts = Post.paginate(:page => params[:page], :per_page => 2, :conditions=>"is_public = 1 #{!logged_in? ? 'AND access_level=0' : ''}", :order=>"posts.created_at DESC")
   end
 end
