b240d7c1bbb8bdc4a7e2424dc7bf1a0601f9e362
Andriy Bazyuta <tihhra@gmail.com>
2009-02-11 03:23:05 +0200

    fg

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(:version => 20090210163758) do
 
   create_table "comments", :force => true do |t|
     t.string   "title",            :limit => 50, :default => ""
-    t.text     "comment",                        :default => ""
+    t.text     "comment"
     t.datetime "created_at",                                     :null => false
     t.integer  "commentable_id",                 :default => 0,  :null => false
     t.string   "commentable_type", :limit => 15, :default => "", :null => false
